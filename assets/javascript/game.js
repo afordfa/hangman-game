413085512d16b11f87a91d26b14b351c963bd3f8
afordfa <anitamford@gmail.com>
2017-02-06 18:50:52 -0600

    began sending javascript back to the DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,33 +4,56 @@
 
 			var animals = ["alligator", "kangaroo", "meerkat", "raccoon", "antelope"];
 			var cities = ["Florence", "Melbourne", "Beijing", "Dallas", "Edinburgh", "San Francisco"];
-			var numberWrong = 0;
-			var guess = "";
-
 
 			var test = "New South Wales";
-			lowerTest = test.toLowerCase();
+			var lowerTest = test.toLowerCase();
 			console.log(lowerTest);
 			var solution = [];
-			var usedLetters = [];
+			var usedLetters = [];					
+			var numberWrong = 0;
+			var guess = "";
+			var displaySolution = $(".gameBoard");
+			var displayLetters = $(".letterTracker");
+
+			$(".resetButton").on("click", function() {
+     			startGame()
+          	});
+			
+			startGame();
 
 
-			//loops through the challenge word and sets the solution variable to underscores
-			//to display to the user as unsolved letters.
-			for (i = 0; i < test.length; i++) {
-				if (test[i] === " "){
-					solution[i] = " ";
-				}
-				else {
-					solution[i] = "_";
-				}
-			};
+			function startGame() {
+				test = "New South Wales";
+				lowerTest = test.toLowerCase();
+				console.log(lowerTest);
+				solution = [];
+				usedLetters = [];					
+				numberWrong = 0;
+				guess = "";
 
-			console.log(solution);
-			console.log(solution.join(" "));
+	
+				//loops through the challenge word and sets the solution variable to underscores
+				//to display to the user as unsolved letters.
+				for (i = 0; i < test.length; i++) {
+					if (test[i] === " "){
+						solution[i] = "\xa0\xa0";
+					}
+					else {
+						solution[i] = "_";
+					}
+				};
 
 
-			console.log(solution.indexOf("_"));
+				
+				$(displaySolution).html(solution.join("\xa0"));
+				console.log(solution.join("\xa0"));
+
+
+				console.log(solution.indexOf("_"));
+			}
+			
+
+
 
 				//ends game after 6 wrong guesses (stickman is drawn):
 						//1 - head
@@ -74,6 +97,7 @@
 						numberWrong++;
 						usedLetters.push(letter.toUpperCase());
 						console.log(usedLetters.sort());
+						$(displayLetters).html(usedLetters.sort().join("\xa0\xa0"));
 						console.log("Wrong  " + solution.join(" "));
 						console.log("misses: " + numberWrong);
 					}
@@ -81,6 +105,7 @@
 					else if (usedLetters.indexOf(letter.toUpperCase()) === -1) {
 
 						usedLetters.push(letter.toUpperCase());
+						$(displayLetters).html(usedLetters.sort().join("\xa0\xa0"));
 						console.log(usedLetters.sort());
 						console.log(numberWrong);
 
@@ -97,19 +122,23 @@
 
 				//after all locations are updated, display the updated solution
 				//to the user.
-					console.log(solution.join(" "));
+				$(displaySolution).html(solution.join("\xa0"));
+				console.log(solution.join("\xa0"));
 
 
 
 				//check number of incorrect guesses to determine if the user has lost the game
 					if (numberWrong === 6) {
-						alert("You Lose!");
+						var replay = alert("You Lose! Click OK to play again!");
+						console.log(replay);
+
+						
 					};
 
 
 				//check solution to see if user has guessed all letters and won the game
 					if (solution.indexOf("_") === -1) {
-						alert("You Win!")
+					console.log(solution.join("\xa0"));alert("You Win!")
 					};
 				};
 			
