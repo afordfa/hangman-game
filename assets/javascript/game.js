66ad99f9a5ca17f02320e9a42e5f5782aa8c3ef5
afordfa <anitamford@gmail.com>
2017-02-09 21:22:35 -0600

    generally working project, needs to be pretty

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,23 @@
 
 		$(document).ready(function() {
 
-			var animals = ["alligator", "kangaroo", "meerkat", "raccoon", "antelope"];
-			var cities = ["Florence", "Melbourne", "Beijing", "Dallas", "Edinburgh", "San Francisco"];
+			var cities = ["Florence", "Melbourne", "Beijing", "Dallas", "Edinburgh", "San Francisco", "New South Wales",
+				"Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch"];
 
-			var test = "New South Wales";
-			var lowerTest = test.toLowerCase();
-			console.log(lowerTest);
+				//https://www.youtube.com/watch?v=fHxO0UdpoxM
+
+				//<iframe width="560" height="315" src="https://www.youtube.com/embed/fHxO0UdpoxM" frameborder="0" allowfullscreen></iframe>
+
+			var currGame = cities[Math.floor(Math.random()*cities.length)];
+			var lowerGame = currGame.toLowerCase();
+			console.log(lowerGame);
 			var solution = [];
 			var usedLetters = [];					
 			var numberWrong = 0;
 			var guess = "";
 			var displaySolution = $(".gameBoard");
 			var displayLetters = $(".letterTracker");
+			var gameOver = "no";
 
 			$(".resetButton").on("click", function() {
      			startGame()
@@ -23,19 +28,21 @@
 
 
 			function startGame() {
-				test = "New South Wales";
-				lowerTest = test.toLowerCase();
-				console.log(lowerTest);
+				currGame = cities[Math.floor(Math.random()*cities.length)];
+				lowerGame = currGame.toLowerCase();
+				console.log(lowerGame);
 				solution = [];
 				usedLetters = [];					
 				numberWrong = 0;
 				guess = "";
+				var gameOver = "no";
+				$(displayLetters).html(usedLetters.sort().join("\xa0\xa0"));
 
 	
 				//loops through the challenge word and sets the solution variable to underscores
 				//to display to the user as unsolved letters.
-				for (i = 0; i < test.length; i++) {
-					if (test[i] === " "){
+				for (i = 0; i < currGame.length; i++) {
+					if (currGame[i] === " "){
 						solution[i] = "\xa0\xa0";
 					}
 					else {
@@ -93,7 +100,7 @@
 				//if it is not, add 1 to the numberWrong variable 
 				//(and draw the next piece of the stick-figure)
 
-					if (lowerTest.indexOf(letter.toLowerCase()) == -1 && usedLetters.indexOf(letter.toUpperCase()) === -1) {
+					if (lowerGame.indexOf(letter.toLowerCase()) == -1 && usedLetters.indexOf(letter.toUpperCase()) === -1) {
 						numberWrong++;
 						usedLetters.push(letter.toUpperCase());
 						console.log(usedLetters.sort());
@@ -113,8 +120,8 @@
 				//everywhere the letter appears.
 
 						for (i = 0; i < solution.length; i++) {
-							if (lowerTest[i] === letter.toLowerCase()) {
-								solution[i] = test[i];
+							if (lowerGame[i] === letter.toLowerCase()) {
+								solution[i] = currGame[i];
 							}
 						}
 
@@ -130,7 +137,7 @@
 				//check number of incorrect guesses to determine if the user has lost the game
 					if (numberWrong === 6) {
 						var replay = alert("You Lose! Click OK to play again!");
-						console.log(replay);
+						gameOver = "lose";
 
 						
 					};
@@ -139,6 +146,7 @@
 				//check solution to see if user has guessed all letters and won the game
 					if (solution.indexOf("_") === -1) {
 					console.log(solution.join("\xa0"));alert("You Win!")
+					gameOver = "win";
 					};
 				};
 			
